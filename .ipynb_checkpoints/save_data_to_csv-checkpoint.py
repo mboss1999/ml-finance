cfb430e5076d25f89cc1e7c5ebd5d26cb163e63d
Michael Boss <mbolila5@gmail.com>
2020-02-03 14:55:01 -0800

    Did Stuff
    Yay

--- .ipynb_checkpoints/save_data_to_csv-checkpoint.py
+++ .ipynb_checkpoints/save_data_to_csv-checkpoint.py
@@ -1,11 +1,27 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
 import json
 
+api_key = 'WS8K5VM8FZL52BTH'
 
-def save_dataset(symbol):
-    api_key = 'WS8K5VM8FZL52BTH'
 
+def save_dataset(symbol, timeframe='daily'):      
+    
     ts = TimeSeries(key=api_key, output_format='pandas')
-    data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+    
+    if timeframe == 'daily':        
+        data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+        data.to_csv(f'./Stock Data/{symbol}_daily_adjusted.csv')
+    elif timeframe == 'intraday':
+        data, meta_data = ts.get_intraday(symbol, '1min', 'full')
+        data.to_csv(f'./Stock Data/{symbol}_intraday_adjusted.csv')
+    else:
+        raise Exception('Unacceptable timeframe. Only daily and intraday are acceptable.')
 
-    data.to_csv(f'./Stock Data/{symbol}_daily_adjusted.csv')
+def save_digital_dataset(symbol, market):
+    
+    cc = CryptoCurrencies(key=api_key, output_format='pandas')
+    
+    data, meta_data = cc.get_digital_currency_daily(symbol, market)
+    
+    data.to_csv(f'./Crypto Data/{symbol}_daily')
